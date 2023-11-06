bf0dd3ede6197fca6ceae9bf6410d4f71ac46e2d
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-06 20:24:41 +0000

    better info

--- getinfo.rb
+++ getinfo.rb
@@ -1,7 +1,8 @@
 require "oauth2"
 require "json"
 require 'colorize'
-
+require 'tty-font'
+require 'tty-box'
 
 UID = "u-s4t2ud-525cf8bc1232d9edf863402ea3b9becefd5945e8a3bd0e0165c72d1ee1e3d067" 
 SECRET = "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"
@@ -11,6 +12,7 @@ def main
         puts "Usage: ruby getinfo.rb <your_login>"
         exit
     end
+    font = TTY::Font.new(:standard)
     login = ARGV[0]
     client = OAuth2::Client.new(UID, 
     SECRET,
@@ -31,13 +33,13 @@ def main
     if (black.empty? && level.empty?)
         puts "No User found"
     else
-        puts "*-------------------------------*"
+        puts "+---------------------------------------------+"
         puts "Hello #{login} :)".white.on_light_blue.blink
-        puts "Grade : #{name}"
-        puts "Available : #{loc}".blue
-        puts "Your level : #{level}".green
+        puts "Grade : #{name}";
+        puts "Available : #{loc}".green
+        puts "Your level : #{level}".blue
         puts "Your blackhole is in #{black}"
-        puts "*-------------------------------*"
+        puts "+---------------------------------------------+"
     end
 end
 
