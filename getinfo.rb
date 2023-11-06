3483629d93f9e4df3c0c8bfc681dbf0c901680fd
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-06 23:59:27 +0100

    Update getinfo.rb

--- getinfo.rb
+++ getinfo.rb
@@ -2,8 +2,8 @@ require "oauth2"
 require "json"
 require 'colorize'
 
-UID = "u-s4t2ud-525cf8bc1232d9edf863402ea3b9becefd5945e8a3bd0e0165c72d1ee1e3d067" 
-SECRET = "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"
+UID = "" 
+SECRET = ""
 
 def main
     if (ARGV.empty? || (UID.empty? || SECRET.empty?))
